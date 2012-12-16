df00a758403023651f12f68b85229af1b8b6409c
ahuhn <anika.huhn@gmail.com>
2012-12-16 09:36:56 -0500

    Adjust concentration, speed parameters

--- Neuron Simulator/neuron_logic_1.js
+++ Neuron Simulator/neuron_logic_1.js
@@ -18,16 +18,18 @@ var svgW = 600,
 	initConIn = 3,
 // 	conOutMultiplier = 100,
 // 	conInMultiplier = 125,
-	conMultiplier = 50,
-	initNumOut = initConOut*conMultiplier,
-	initNumIn = initConIn*conMultiplier,
-	numberPassedIn = 0,
+	initConMultiplier = 100,
+	initNumOut = initConOut*initConMultiplier,
+	initNumIn = initConIn*initConMultiplier,
+	numberAddedIn = 0,
+	numberAddedOut = 0,
 	currentV = initV,
 // 	constantR = 8.314,
 // 	constantz = 1,
 // 	constantF = 96485,
-	constantCalculated = 5,
-	randSpeed = 15,
+// 	constantActual = constantR*initT/constantz/constantF,
+	constantCalculated = 0.5,
+	randSpeed = 3,
 	ionGateSpacing = 80
 	ionGateW = 2; // also update mod 0, 1, 2, 3 and other stuff(not implemented yet)
 
@@ -46,22 +48,6 @@ var svg = d3.select("svg")
 // 	.attr("fill", "none")
 // 	.call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom));
 
-var neuronBox = svg.append("rect")
-	.attr("width", svgW)
-	.attr("height", svgH)
-	.attr("fill", "none")
-	.attr("stroke", "black")
-	.attr("stroke-width", "4");
-
-for(var i = 0; i < svgW/ionGateSpacing; i++) {
-	svg.append("rect")
-		.attr("transform", "translate(" + (ionGateSpacing*i+4) + ", " + (svgH/2 - membraneT/2) + ")")
-		.attr("width", ionGateSpacing-5)
-		.attr("height", membraneT)
-		.attr("fill", "none")
-		.attr("stroke", "green")
-		.attr("stroke-width", "2");
-}
 
 // var key = svg.append("rect")
 // 	.attr("width", 100)
@@ -107,67 +93,19 @@ var keyTextV = neuronKey.append("text")
 	.text(currentV);
 var keyTextNum = neuronKey.append("text")
 	.attr("transform", "translate(82, 95)")
-	.text(numberPassedIn);
+	.text(numberAddedIn);
 
 
-   
 
-// var neuronKey = d3.select("body").append("div")
-// 	.attr("position", "relative")
-// 	.attr("top", "-500px")
-// 	.attr("left", "700px")
-// 	.attr("width", keyW)
-// 	.attr("height", keyH)
-//    .append("svg")
-// 	.attr("width", keyW)
-// 	.attr("height", keyH)
-//    .append("g");
-// var neuronKey = d3.select("#divForKey").append("svg")
-// 	.attr("width", keyW)
-// 	.attr("height", keyH)
-//    .append("g");
-// 
-// var keyRect = neuronKey.append("rect")
-// 	.attr("width", keyW)
-// 	.attr("height", keyH)
-// 	.attr("fill", "none")
-// 	.attr("stroke", "black")
-// 	.attr("stroke-width", "4");
-// 
-// var keyText1 = neuronKey.append("text")
-// 	.attr("transform", "translate(10, 20)")
-// 	.text("Membrane");
-// 
-// var keyText2 = neuronKey.append("text")
-// 	.attr("transform", "translate(10, 40)")
-// 	.text("Na+ ions");
-// 
-// var keyDot2 = neuronKey.append("ellipse")
-// 	.attr("cx", "100")
-// 	.attr("cy", "35")
-// 	.attr("rx", "6")
-// 	.attr("ry", "6")
-// 	.attr("stroke", "black")
-// 	.attr("fill", "yellow");
-// 
-// var keyText3 = neuronKey.append("text")
-// 	.attr("transform", "translate(10, 60)")
-// 	.attr("stroke", "lilac")
-// 	.text("K+ ions");
-// 
-// var keyDot3 = neuronKey.append("ellipse")
-// 	.attr("cx", "90")
-// 	.attr("cy", "55")
-// 	.attr("rx", "6")
-// 	.attr("ry", "6")
-// 	.attr("stroke", "black")
-// 	.attr("fill", "#C8A2C8");
-	
 var ionG1 = d3.select("svg").append("g").selectAll("circle")
 	.data(particleState)
 	.enter().append("circle")
+// 	.attr("stroke", "none")
 	.attr("stroke", "black")
-	.attr("fill", "gray")
+	.attr("fill", 'rgb(200,100,200)')
+// 	.attr("fill", 'rgba(200,0,200, 0.05)')
+// 	.attr("fill", 'url(#grad1)')
+// 	.attr("r", 70)
 	.attr("r", ionR)
 	.attr("cx", function(d) {
 		return d[0];
@@ -176,6 +114,27 @@ var ionG1 = d3.select("svg").append("g").selectAll("circle")
 		return d[1];
 		});
 
+
+for(var i = 0; i < svgW/ionGateSpacing; i++) {
+	svg.append("rect")
+		.attr("transform", "translate(" + (ionGateSpacing*i+4) + ", " + (svgH/2 - membraneT/2) + ")")
+		.attr("width", ionGateSpacing-5)
+		.attr("height", membraneT)
+		.attr("fill", "white")
+		.attr("stroke", "green")
+		.attr("stroke-width", "2");
+}
+
+var neuronBox = svg.append("rect")
+	.attr("width", svgW)
+	.attr("height", svgH)
+	.attr("fill", "none")
+	.attr("stroke", "black")
+	.attr("stroke-width", "4");
+
+
+neuronBox.on('click', "function(){console.log('aaaaa');}");
+	
 // function zoom() {
 // 	console.log("zooming");
 // 	svg.select("g")
@@ -183,6 +142,8 @@ var ionG1 = d3.select("svg").append("g").selectAll("circle")
 // }
 
 function move() {
+	// find which ions the force will mostly act on
+	
 // 	console.log('moving');
 	for(var i = 0; i < particleState.length; i++) {
 		var pState = particleState[i];
@@ -199,46 +160,46 @@ function move() {
 		// deal with y-speed
 		// first, test if the ion should shoot through an ion channel
 		if((Math.floor(pState[0])%ionGateSpacing == 0 || Math.floor(pState[0])%ionGateSpacing == 1)
-			 && pState[1]>=svgH/2 - membraneT/2-5 && pState[1]<=svgH/2 + membraneT/2 +5){
-			//pState[1] = (membraneT/2+7)*pState[3]/Math.abs(pState[3]) + svgH/2;
-			if(pState[3]<0){
-				pState[1] = svgH/2 - (membraneT/2+7);
-				numberPassedIn -= 1;
+			 && pState[1]>=svgH/2 - membraneT/2-ionR-3 && pState[1]<=svgH/2 + membraneT/2 +ionR+3){
+			if(pState[3]<0 && pState[1]>svgH/2 || pState[3]>0 && pState[1]>=svgH/2){
+				pState[1] = svgH/2 - (membraneT/2+ionR+5);
+				numberAddedIn -= 1; // instead of counting these here, calculate [ion]in, out after movement
+				numberAddedOut += 1;
 			} else {
-				pState[1] = svgH/2 + (membraneT/2+7);
-				numberPassedIn += 1;
+				pState[1] = svgH/2 + (membraneT/2+ionR+5);
+				numberAddedIn += 1;
+				numberAddedOut -= 1;
 			}
 		// next, test if the ion should bounce off the wall of the membrane
-		} else if(pState[1]+ionR>=svgH/2 - membraneT/2 && pState[1]-ionR<=svgH/2 + membraneT/2){
+		} else if(pState[1]>=svgH/2 - membraneT/2-ionR-2 && pState[1]<=svgH/2 + membraneT/2 +ionR+2){
 			if(pState[3]<0){
-				pState[1] = svgH/2 + (membraneT/2+1) + ionR;
+				pState[1] = svgH/2 + (membraneT/2+1) + ionR+2;
 			} else {
-				pState[1] = svgH/2 - (membraneT/2+1) - ionR;
+				pState[1] = svgH/2 - (membraneT/2+1) - ionR-2;
 			}
 			pState[3] *= -1;
 		// next, check if the ball stays in the area.
-		} else if(pState[1]>0-ionR && pState[1]<svgH/2 - membraneT/2 || pState[1]>svgH/2 + membraneT/2 && pState[1]<svgH+ionR) {
-// 			if(i == 0){
-// 				console.log(pState[0]+ ', ' +pState[1]);
-// 				console.log(pState[3]);
-// 				console.log(currentV);
-// 				console.log(currentV/ Math.pow(pState[1]-svgH/2, 2));
-// 				}
+		} else if(pState[1]>=-1-ionR && pState[1]<svgH/2 - membraneT/2-ionR-2 || pState[1]>svgH/2 + membraneT/2 +ionR+2 && pState[1]<=svgH+ionR+1) {
 			pState[3] += currentV / Math.pow(pState[1]-svgH/2, 2); // *********** change this.  also, don't bounce off walls.  have things come in at the correct temperature.
-// 			pState[3] += currentV / ((pState[1]-svgH/2)^2) // *********** change this.  also, don't bounce off walls.  have things come in at the correct temperature.
 			pState[1] = pState[1]+pState[3];
 		// next, make throw in a new ion if this one went out of bounds
 		} else {
-			pState[1] = pState[1]-pState[3];
-// 			pState[3] = -pState[3];
-			pState[0] = Math.random()*svgW;
-			pState[3] = -pState[3]/Math.abs(pState[3])*Math.random()*randSpeed/2;
+// 			pState[0] = Math.random()*svgW;
+			if(pState[1]<-1-ionR){
+				pState[1] = -1-ionR;
+				pState[3] = Math.random()*randSpeed/2;
+			} else if(pState[1]>svgH+ionR+1){
+				pState[1]=svgH+ionR+1;
+				pState[3] = -Math.random()*randSpeed/2;
+			} else {
+				console.log("error: lost ball");
+			}
 		}
 		
 		// V = Q/C
 		// V = qe/C
 		// \delta V = \delta q * e/C
-		currentV = -constantCalculated*initT*Math.log(initConOut/initConIn*(initNumIn+numberPassedIn)/(initNumOut-numberPassedIn)) + initV;
+		currentV = -constantCalculated*initT*Math.log(initConOut/initConIn*(initNumIn+numberAddedIn)/(initNumOut+numberAddedOut)) + initV;
 		//console.log(currentV);
 		particleState[i] = pState;
 	}
@@ -255,30 +216,33 @@ function move() {
 			return d[1];
 			});
 
-	circles.transition()
-		.duration(300)
-		.ease("linear")
-		.attr("cx", function(d) {
-			return d[0];
-			})
-		.attr("cy", function(d) {
-			return d[1];
-			});
-			
-// 	circles.attr("cx", function(d) {
+// 	circles.transition()
+// 		.duration(240)
+// 		.ease("linear")
+// 		.attr("cx", function(d) {
 // 			return d[0];
 // 			})
 // 		.attr("cy", function(d) {
 // 			return d[1];
 // 			});
+			
+	circles.attr("cx", function(d) {
+			return d[0];
+			})
+		.attr("cy", function(d) {
+			return d[1];
+			});
 
 
 	circles.exit().remove();
+	// 
+// 	svg.selectAll("rect")
+// 		.attr('stroke', 'black');
 	
-	keyTextIn.text((initNumIn+numberPassedIn)/conMultiplier);
-	keyTextOut.text((initNumOut-numberPassedIn)/conMultiplier);
+	keyTextIn.text((initNumIn+numberAddedIn)/initConMultiplier);
+	keyTextOut.text((initNumOut+numberAddedOut)/initConMultiplier);
 	keyTextV.text(currentV*10);
-	keyTextNum.text(numberPassedIn);
+	keyTextNum.text(numberAddedIn);
 	
 }
 
